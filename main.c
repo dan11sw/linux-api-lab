a2b0f925c0f4d8b7f289d90a435286646f38198d
dan11sw <swdan.ov@gmail.com>
2025-08-13 16:00:34 +0800

    feat: update custom_tee.c

--- main.c
+++ main.c
@@ -6,20 +6,60 @@
 const int BUF_SIZE = 100;
 
 void customTee(int fd_out) {
-    char buf[BUF_SIZE];
+    char buf[BUF_SIZE + 1];
+    char zero_with_sep[2] = {'\0', '\n'};
+    char zero[1] = {'\0'};
+
     ssize_t numRead;
+
+    // Zeroing an array of characters
+    memset(buf, 0, BUF_SIZE);
 
     // Read data from standart input
     numRead = read(STDIN_FILENO, &buf, BUF_SIZE);
+    buf[BUF_SIZE] = '\0';
 
     if(numRead == -1) {
         errExit("read");
     }
 
-    // Write to file
+    // Delete zero element
+    numRead -= 1;
+
+    // Write buf to file
     if(write(fd_out, buf, numRead) != numRead) {
         fatal("couldn't write whole buffer");
     }
+
+    if(lseek(fd_out, -1, SEEK_END) == -1) {
+        errExit("lseek");
+    }
+
+    char readBuf[1];
+    ssize_t readCount = read(fd_out, readBuf, 1);
+    
+    if(readCount != 1) {
+        fatal("couldn't read whole buffer");
+    }
+
+    if(readBuf[0] != '\n') {
+        if(lseek(fd_out, 0, SEEK_END) == -1) {
+            errExit("lseek");
+        }
+
+        if(readBuf[0] == '\0') {
+            if(write(fd_out, zero, 1) != 1) {
+                fatal("couldn't write whole buffer");
+            }
+        } else {
+            if(write(fd_out, zero_with_sep, 2) != 2) {
+                fatal("couldn't write whole buffer");
+            }
+        }
+    }
+
+    // Add zero element
+    numRead += '\0';
 
     // Write to standart output
     if(write(STDOUT_FILENO, buf, numRead) != numRead) {
@@ -62,15 +102,11 @@ int main(int argc, char* argv[]) {
     }
 
     if(indexFile == 2) {
-        printf("Append to file");
-
         // To end file
-        /*if(lseek(fd, 0, SEEK_END) == -1) {
+        if(lseek(fd, 0, SEEK_END) == -1) {
             errExit("lseek");
-        }*/
+        }
     }
-
-    printf("\nHello, world!");
     
     customTee(fd);
 
